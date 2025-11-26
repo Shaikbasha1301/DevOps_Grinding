6e603711e46405f335c5c250bc4bad4c1eaadb0e
Shaik Rehan Basha <119519167+Shaikbasha1301@users.noreply.github.com>
2025-11-26 18:55:40 +0530

    Update one.sh

--- Basha_Scripting/one.sh
+++ Basha_Scripting/one.sh
@@ -1,37 +1,37 @@
 #! /bin/bash
 
 
-##variables.....................
+#variables
 
-#name="rehan"
-#echo -e "welocme to bash scripting \n myname is $name \tstoicism"
-
-
-##runtime vairables......................
-
-#echo "enter your username"
-#read username
-#echo "my username is $username"
-#echo "enter password"
-#read passwd
-#echo "it is your $passwd"
-
-##Silent Proxy
-
-#echo "Enter your name"
-#read name
-#echo "your $name"
-#read -sp "enter your password" pass
-#echo "this is your $pass"
+name="rehan"
+echo -e "welocme to bash scripting \n myname is $name \tstoicism"
 
 
-##Argumets
+#runtime vairables
 
-#echo "my name is $1"
-#echo "I'm from $2"
-#echo "workinf as an $3"
-#echo "print all the argumrnts $@"
-#echo "file $0"
+echo "enter your username"
+read username
+echo "my username is $username"
+echo "enter password"
+read passwd
+echo "it is your $passwd"
+
+#Silent Proxy
+
+echo "Enter your name"
+read name
+echo "your $name"
+read -sp "enter your password" pass
+echo "this is your $pass"
+
+
+#Argumets
+
+echo "my name is $1"
+echo "I'm from $2"
+echo "workinf as an $3"
+#cho "print all the argumrnts $@"
+echo "file $0"
 
 
 
